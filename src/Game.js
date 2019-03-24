8ae597d7ad365792ce7d7447e0d370cb4506921b
Michael Handria <michaelhandria@gmail.com>
2019-03-23 17:40:32 -0700

    [feat][style] time traverse and style update

--- src/Game.js
+++ src/Game.js
@@ -2,18 +2,95 @@ import React, { Component } from 'react';
 import Board from './Board';
 import './Game.css';
 
+
 class Game extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            isCircle: false,
+        }
+    }
+
+    jumpTo(step) {
+        const history = this.state.history.slice(0, step + 1);
+        this.setState({
+            history: history,
+            stepNumber: step,
+            isCircle: (step%2) !== 0
+        })
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if(this.calculateWinner(squares) || squares[i] !== null) {
+            return;
+        }
+        squares[i] = (this.state.isCircle) ? 'O' : 'X';
+
+        this.setState({
+            history: history.concat({
+                squares: squares
+            }),
+            stepNumber: history.length,
+            isCircle: !this.state.isCircle,
+        });
+    }
+
+    calculateWinner(squares){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = this.calculateWinner(current.squares);
+
+        let status = `Next player: ${(this.state.isCircle) ? 'O' : 'X'}`;
+        status = (winner) ? `Winner: ${winner}` : status;
+
+        const moves = history.map((step, move) => {
+            const desc = move ? `Go to move ${move}` : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
         return (
-        <div className="game">
-            <div className="game-board">
-                <Board />
+            <div className="game">
+                <div className="game-board">
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}/>
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
             </div>
-            <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
-            </div>
-        </div>
         );
     }
 }
